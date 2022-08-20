87f3b3dff8133618b95812dc17903bd5056b27ba
Paul Fernandez <ferpaul1974@gmail.com>
2022-08-20 14:40:16 -0400

    test

--- Gf.js
+++ Gf.js
@@ -7,3 +7,21 @@ fetch('https://love-quote-api.herokuapp.com/Love') //create or find an api for l
 })
 
 
+let catButton = document.getElementById('get-cat');
+catButton.addEventListener("click", evt =>{
+
+	let catDiv = document.getElementById('cat-pic')
+
+	fetch('https://api.thecatapi.com/v1/images/search?')
+	.then(res => res.json())
+	.then(cats => {
+		cats.forEach(cat => {
+			catDiv.innerHTML = `<h3>Here is this cute cat wishing you a good day! </h3>
+			<img src="${cat.url}" alt="kitty" />`
+		});
+	})
+
+
+
+})
+
